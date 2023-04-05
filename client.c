bc7b98032fa3662200d3750b1422454cb430d437
nick298740 <nick.meyer14@gmail.com>
2023-04-05 00:00:43 -0500

    Update client.c
    Updated to work on local machine

--- client.c
+++ client.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+
+#define IP_ADDRESS 127.0.0.1
+
 int sockfd, n, PORT_NUMBER;
 int len = sizeof(struct sockaddr);
 char recvline[40960];
@@ -41,7 +43,7 @@ void connectToServer(){
     servaddr.sin_port=htons(PORT_NUMBER); // Server port number
  
     /* Convert IPv4 and IPv6 addresses from text to binary form */
-	inet_pton(AF_INET,"129.120.151.94",&(servaddr.sin_addr));
+	inet_pton(AF_INET,IP_ADDRESS,&(servaddr.sin_addr));
  
     /* Connect to the server */
     connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
@@ -55,18 +57,14 @@ int main(int argc,char **argv){
 
 	printf("\n\nPlease enter the URL to request from proxy server, using this form : www.example.com\n\n");
 	
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//////// Taken from https://stackoverflow.com/questions/16376817/get-the-whole-input-of-a-string-in-c ////////////
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	char option[64],line[256];
-
-    printf(">> ");
-    if(fgets(line, sizeof(line), stdin)) {
-        if (1 == sscanf(line, "%[^\n]%*c", option)) {//[^\n] isn't newline chars
-			 strcpy(buffer, option);
+        printf(">> ");
+        if(fgets(line, sizeof(line), stdin)) {
+            if (1 == sscanf(line, "%[^\n]%*c", option)) {//[^\n] isn't newline chars
+	        strcpy(buffer, option);
+	
+	    }
         }
-    }
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	printf("\n\nHere is the message written to the server: %s\n\n", buffer);
 	writeMessage();
